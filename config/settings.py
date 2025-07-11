c909e3002bb7864c890e2c2e906cf7991266d7dd
giuseppe cirino <giuseppecirino@outlook.be>
2025-07-11 17:08:32 +0200

    Reset migrations

--- config/settings.py
+++ config/settings.py
@@ -178,4 +178,4 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'pepecirino@gmail.com' # Mets bien ton adresse Gmail ici
 EMAIL_HOST_PASSWORD = 'adyqffbrcwxwvdew' # Mets ton mot de passe d’application SANS espace
 DEFAULT_FROM_EMAIL = 'pepecirino@gmail.com'
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
